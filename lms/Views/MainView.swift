d8f3930d2659c142deab86f0c175d9f5ab6f546a
Ivan Isaev <blazer@mifors.com>
2025-06-17 16:06:18 +0400

    Удалить дефолтные файлы, сгенерированные при создании проекта

---
+++ lms/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  lms
+//
+//  Created by Ivan Isaev on 17.06.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+  
+  var body: some View {
+    
+  }
+  
+  
+}
+
+#Preview {
+  MainView()
+}
