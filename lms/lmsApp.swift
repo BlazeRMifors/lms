69873ddfa2d4030da03c4c0a182af53a8b9464c1
Ivan Isaev <blazer@mifors.com>
2025-06-20 21:26:58 +0400

    Удалить файлы домашки 2, оставить только вспомогательный код для чистоты

--- lms/lmsApp.swift
+++ lms/lmsApp.swift
@@ -10,30 +10,9 @@ import SwiftUI
 @main
 struct lmsApp: App {
   
-  init() {
-    // Настройка общего стиля NavigationBar
-    let appearance = UINavigationBarAppearance()
-    appearance.configureWithTransparentBackground()
-//      .configureWithDefaultBackground()
-    
-    // Цвет кнопок навигации (включая стрелку назад)
-    appearance.backButtonAppearance.normal.titleTextAttributes[.foregroundColor] = UIColor.red
-    
-    // Также можно задать цвет значков
-//    appearance.backButtonAppearance.normal.iconColor = UIColor.red
-    
-    // Установка как для всех навигационных баров
-    UINavigationBar.appearance().standardAppearance = appearance
-    UINavigationBar.appearance().compactAppearance = appearance
-    UINavigationBar.appearance().scrollEdgeAppearance = appearance
-    
-    // Для поддержки iOS 15 и ниже
-    UINavigationBar.appearance().tintColor = .red
-  }
-  
   var body: some Scene {
     WindowGroup {
-      MainTabView()
+      Text("В разработке")
     }
   }
 }
