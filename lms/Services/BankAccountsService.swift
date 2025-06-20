220ccebfbcc8cd33728b85ac446e7cd81accb2eb
Ivan Isaev <blazer@mifors.com>
2025-06-20 20:44:46 +0400

    Добавить фабрику транзакций для отладки верстки

--- lms/Services/BankAccountsService.swift
+++ lms/Services/BankAccountsService.swift
@@ -7,23 +7,28 @@
 
 import Foundation
 
+protocol BankAccountsServiceProtocol {
+  func getUserAccount() async -> BankAccount
+  func updateAccount(balance: Decimal, currency: Currency) async
+}
+
 final class BankAccountsService {
-    
-    private var mockAccount = BankAccount(
-        id: 1,
-        balance: 1000.00,
-        currency: .rub
+  
+  private var mockAccount = BankAccount(
+    id: 1,
+    balance: 1000.00,
+    currency: .rub
+  )
+  
+  func getUserAccount() async -> BankAccount {
+    return mockAccount
+  }
+  
+  func updateAccount(balance: Decimal, currency: Currency) async {
+    mockAccount = BankAccount(
+      id: mockAccount.id,
+      balance: balance,
+      currency: currency
     )
-    
-    func getUserAccount() async -> BankAccount {
-        return mockAccount
-    }
-    
-    func updateAccount(balance: Decimal, currency: Currency) async {
-        mockAccount = BankAccount(
-            id: mockAccount.id,
-            balance: balance,
-            currency: currency
-        )
-    }
+  }
 }
--- lms/Services/TransactionsService.swift
+++ lms/Services/TransactionsService.swift
@@ -7,35 +7,43 @@
 
 import Foundation
 
+protocol TransactionsServiceProtocol {
+  func getTransactions(for direction: Direction, in period: DateInterval) async -> [Transaction]
+  func create(transaction: Transaction) async
+  func update(transaction: Transaction) async
+  func delete(withId id: Int) async
+}
+
 final class TransactionsService {
-    
-    // MARK: - Свойства инстанса
-    
-    private var cache = TransactionsFileCache()
-    private let cacheName = "defaultTransactionCache"
-    
-    init() {
-        cache.load(from: cacheName)
+  
+  // MARK: - Свойства инстанса
+  
+  private var cache = TransactionsFileCache()
+  private let cacheName = "defaultTransactionCache"
+  
+  init() {
+    cache.load(from: cacheName)
+    cache.generateTransactions()
+  }
+  
+  func getTransactions(for direction: Direction, in period: DateInterval) async -> [Transaction] {
+    cache.transactions.filter {
+      $0.category.direction == direction && period.contains($0.transactionDate)
     }
-    
-    func getTransactions(for period: DateInterval) async -> [Transaction] {
-        cache.getAll().filter {
-            period.contains($0.transactionDate)
-        }
-    }
-    
-    func create(transaction: Transaction) async {
-        cache.insert(transaction)
-        cache.save(to: cacheName)
-    }
-    
-    func update(transaction: Transaction) async {
-        cache.insert(transaction)
-        cache.save(to: cacheName)
-    }
-    
-    func delete(withId id: Int) async {
-        cache.remove(withId: id)
-        cache.save(to: cacheName)
-    }
+  }
+  
+  func create(transaction: Transaction) async {
+    cache.insert(transaction)
+    cache.save(to: cacheName)
+  }
+  
+  func update(transaction: Transaction) async {
+    cache.insert(transaction)
+    cache.save(to: cacheName)
+  }
+  
+  func delete(withId id: Int) async {
+    cache.remove(withId: id)
+    cache.save(to: cacheName)
+  }
 }
