a311359d19ea6bbe525d258ca16bfeb624e757b3
Ivan Isaev <blazer@mifors.com>
2025-07-04 12:00:12 +0400

    Форматировать текст

--- lms/Views/Categories/CategoriesView.swift
+++ lms/Views/Categories/CategoriesView.swift
@@ -1,43 +1,48 @@
+//
+//  CategoriesView.swift
+//  lms
+//
+//  Created by Ivan Isaev on 03.07.2025.
+//
+
 import SwiftUI
 
 struct CategoriesView: View {
-    @State var viewModel: CategoriesViewModel
-    
-    var body: some View {
-        NavigationStack {
-            List {
-                Section(header: Text("статьи")
-                  .font(.subheadline)
-                  .padding(.leading, 0)
-                ) {
-                    ForEach(viewModel.filteredCategories) { category in
-                        HStack {
-                            Text(String(category.emoji))
-                            .font(.footnote)
-                            .padding(4)
-                            .background(
-                              Circle().fill(Color.accent.opacity(0.2))
-                            )
-                            Text(category.name)
-                            .padding(.leading, 6)
-                        }
-                        .alignmentGuide(.listRowSeparatorLeading) { _ in
-                          40
-                        }
-                    }
-                }
+  @State var viewModel: CategoriesViewModel
+  
+  var body: some View {
+    NavigationStack {
+      List {
+        Section(header: Text("статьи")
+          .font(.subheadline)
+        ) {
+          ForEach(viewModel.filteredCategories) { category in
+            HStack {
+              Text(String(category.emoji))
+                .font(.footnote)
+                .padding(4)
+                .background(
+                  Circle().fill(Color.accent.opacity(0.2))
+                )
+              Text(category.name)
+                .padding(.leading, 6)
             }
-            .listStyle(.insetGrouped)
-            .navigationTitle("Мои статьи")
-            .searchable(text: $viewModel.searchText, placement: .navigationBarDrawer(displayMode: .always))
-            .onChange(of: viewModel.searchText) { _, newValue in
-                viewModel.updateSearch(text: newValue)
+            .alignmentGuide(.listRowSeparatorLeading) { _ in
+              40
             }
-            .onAppear {
-                Task { await viewModel.loadCategories() }
-            }
+          }
         }
+      }
+      .navigationTitle("Мои статьи")
+      .searchable(text: $viewModel.searchText, placement: .navigationBarDrawer(displayMode: .always))
+      .onChange(of: viewModel.searchText) { _, newValue in
+        viewModel.updateSearch(text: newValue)
+      }
+      .onAppear {
+        Task { await viewModel.loadCategories() }
+      }
     }
+  }
 }
 
 #Preview {
