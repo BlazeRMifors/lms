919a35987bf80f92d4f5245b3a39488e0f44087e
Ivan Isaev <blazer@mifors.com>
2025-07-04 11:09:12 +0400

    Доработать верстку списка

--- lms/Views/Categories/CategoriesView.swift
+++ lms/Views/Categories/CategoriesView.swift
@@ -6,12 +6,23 @@ struct CategoriesView: View {
     var body: some View {
         NavigationStack {
             List {
-                Section(header: Text("статьи")) {
+                Section(header: Text("статьи")
+                  .font(.subheadline)
+                  .padding(.leading, 0)
+                ) {
                     ForEach(viewModel.filteredCategories) { category in
                         HStack {
                             Text(String(category.emoji))
-                                .font(.title2)
+                            .font(.footnote)
+                            .padding(4)
+                            .background(
+                              Circle().fill(Color.accent.opacity(0.2))
+                            )
                             Text(category.name)
+                            .padding(.leading, 6)
+                        }
+                        .alignmentGuide(.listRowSeparatorLeading) { _ in
+                          40
                         }
                     }
                 }
@@ -19,7 +30,7 @@ struct CategoriesView: View {
             .listStyle(.insetGrouped)
             .navigationTitle("Мои статьи")
             .searchable(text: $viewModel.searchText, placement: .navigationBarDrawer(displayMode: .always))
-            .onChange(of: viewModel.searchText) { newValue in
+            .onChange(of: viewModel.searchText) { _, newValue in
                 viewModel.updateSearch(text: newValue)
             }
             .onAppear {
