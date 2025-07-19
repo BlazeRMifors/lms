797f958b94de288e505d2adcf64eb980ee1bff0b
Isaev Ivan <blazer@mifors.com>
2025-07-19 08:30:18 +0400

    Миграция

--- lms/Helpers/StorageType.swift
+++ lms/Helpers/StorageType.swift
@@ -27,4 +27,34 @@ func makeTransactionsStorage() -> TransactionsStorage {
     case .coredata:
         return TransactionsCoreDataStorage()
     }
+}
+
+func migrateTransactionsStorageIfNeeded() async {
+    let lastType = UserDefaults.standard.string(forKey: "last_storage_type") ?? "file"
+    let currentType = StorageType.current().rawValue
+    guard lastType != currentType else { return }
+    
+    let oldType = StorageType(rawValue: lastType) ?? .file
+    let newType = StorageType.current()
+    
+    let oldStorage: TransactionsStorage = {
+        switch oldType {
+        case .file: return TransactionsSwiftDataStorage() // заменить на файловую реализацию, если есть
+        case .swiftdata: return TransactionsSwiftDataStorage()
+        case .coredata: return TransactionsCoreDataStorage()
+        }
+    }()
+    let newStorage: TransactionsStorage = makeTransactionsStorage()
+    
+    let allOld = await oldStorage.all()
+    for tx in allOld {
+        await newStorage.insert(tx)
+    }
+    await newStorage.save()
+    // Очищаем старое хранилище
+    for tx in allOld {
+        await oldStorage.remove(id: tx.id)
+    }
+    await oldStorage.save()
+    UserDefaults.standard.set(currentType, forKey: "last_storage_type")
 } 
