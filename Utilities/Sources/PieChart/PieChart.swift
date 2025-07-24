90a5f54ac2b43e160980308dc0da65acccb13d71
Ivan Isaev <blazer@mifors.com>
2025-07-24 20:50:00 +0400

    Добавить модуль для отрисовки графика PieChart

---
+++ Utilities/Sources/PieChart/PieChart.swift
@@ -0,0 +1,14 @@
+//
+//  PieChart.swift
+//  Utilities
+//
+//  Created by Ivan Isaev on 24.07.2025.
+//
+
+import Foundation
+
+public struct PieChart {
+  
+    public static let version = "1.0.0"
+    public init() {}
+}
